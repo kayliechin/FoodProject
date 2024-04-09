5b4bbc32fe5a6445aa121c5155e221268fd4e406
Kaylie <kc78988@uga.edu>
2024-04-09 19:36:03 -0400

    Added Thanks!-Kaylie to the exit program println-Kaylie Chin

--- src/Main.java
+++ src/Main.java
@@ -95,6 +95,7 @@ public class Main {
                     break;
                 default:
                     System.out.println("Exit from the program");
+                    System.out.println("Thanks!-Kaylie");
                     return;
             }
         }
